a372e4be733e047afc629702db07c4862de1b3c0
Kimin Ryu <ryu@holix.com>
2021-11-19 23:41:50 +0900

    Jetpack Compose Basics 7

--- week 1-Jetpack Compose basics/Compose/app/src/main/java/me/workspace/compose/MainActivity.kt
+++ week 1-Jetpack Compose basics/Compose/app/src/main/java/me/workspace/compose/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,22 +33,27 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
-                .weight(1F)) {
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
