5af0d41f4af0380fdd53b4b63e88d9627c708a92
Kimin Ryu <ryu@holix.com>
2021-12-10 19:58:29 +0900

    Layouts in Jetpack Compose 9

--- week 2-1-Layouts in JetpackCompose/ComposeLayout/app/src/main/java/me/workspace/composelayout/StaggeredGrid.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayout/app/src/main/java/me/workspace/composelayout/StaggeredGrid.kt
@@ -121,7 +121,13 @@ val topics = listOf(
 
 @Composable
 fun StaggeredGridSample(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState())
+    ) {
         StaggeredGrid(modifier = modifier) {
             for (topic in topics) {
                 Chip(modifier = Modifier.padding(8.dp), text = topic)
