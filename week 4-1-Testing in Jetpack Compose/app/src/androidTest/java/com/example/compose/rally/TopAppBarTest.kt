a00dbd20ed29fd0c184d24e733fbe85a918c5bef
Kimin Ryu <ryu@holix.com>
2021-12-10 23:09:33 +0900

    Testing in Jetpack Compose 5

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -11,19 +11,6 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun rallyTopAppBarTest() {
-        val allScreens = RallyScreen.values().toList()
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
-        }
-        Thread.sleep(5000)
-    }
-
-    @Test
     fun rallyTopAppBarTest_currentTabSelected() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
@@ -51,7 +38,13 @@ class TopAppBarTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
